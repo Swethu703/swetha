4039ebbb466c4e93ed978a0df51e44ed044bf42b
Swethu703 <swethacs703@gmail.com>
2025-05-14 22:49:51 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# import streamlit as st
+# st.title("ヾ( ˃ᴗ˂ )◞ • *✰helloヾ( ˃ᴗ˂ )◞ • *✰")
+# st.header("_python progam_")
+# st.subheader("by swetha")
+# st.text("welcome to new program")
+# st.write("😊end of  program line😊")
+# st.write("## written by swetha")
+# st.badge("home",color="green")
+# st.divider()
+# st.code("""# This program adds two numbers
+
+# num1 = 1.5
+# num2 = 6.3
+
+# # Add two numbers
+# sum = num1 + num2
+
+# # Display the sum
+# print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# """,language="python")
+# st.divider()
+# with st.sidebar:
+#     st.write("button 1:")
+#     st.divider()
+#     st.write("this is a sidebar:")
